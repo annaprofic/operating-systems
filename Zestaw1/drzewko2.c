d552036f09b74cb08903732c9f0406d455a53633
Anna <33325994+sarnavskaa@users.noreply.github.com>
2018-11-05 14:33:46 +0100

    Update drzewko2.c

--- Zestaw1/drzewko2.c
+++ Zestaw1/drzewko2.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "procinfo.h" /* plik naglowkowy */
+
 int dzieci = 0; /* zmienna ktora liczy ilosc dzieci */
 int i = 0;
 
@@ -15,18 +16,18 @@ int main(int argc, char *argv[]) {
 		switch (fork()) {
 			case -1: /* fork zwraca wartosc -1 ktora oznacza porazke */
 								/* Powodem moze byc brak pamieci lub przekroczenie limitu na liczbe procesow */
-	      perror("Nowego procesu potomnego nie da sie utworzyc.\n");
-	      exit(0);
-				break;
+			      perror("Nowego procesu potomnego nie da sie utworzyc.\n");
+			      exit(0);
+			      break;
 			case 0: /* kiedy istnieje proces potomny - wypisuje id procesow */
-	      strncpy(argv[0], "Child", strlen(argv[0]));
-				procinfo(argv[0]);
-	      break;
+			      strncpy(argv[0], "Child", strlen(argv[0]));
+			      procinfo(argv[0]);
+	      		      break;
 			default:
-	      dzieci ++; /* musimy policzyc ile procesow potomnych sie tworza, aby pozniej proces miecierzysty czekal potrzebna ilosc razy */
-	      strcpy(argv[0], "Parent");
-				break;
-	  }
+			      dzieci ++; /* musimy policzyc ile procesow potomnych sie tworza, aby pozniej proces miecierzysty czekal potrzebna ilosc razy */
+			      strcpy(argv[0], "Parent");
+			      break;
+	  	}
 	  if (dzieci != 0 ) { /* jezeli ilosc procesow potomnych rozni sie od zera to prosec macierzysty czeka */
 	    wait(NULL); /* wylolanie znajduje sie w petli wiec odbedzie sie potrzebna ilosc razy */
 	    break;
